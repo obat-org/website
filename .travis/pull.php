aedafa695b846de5b3117c141140b2ff6a3749e8
Toby Batch <tobias@neontribe.co.uk>
2019-07-10 08:58:01 +0100

    Fixed pull script

--- .travis/pull.php
+++ .travis/pull.php
@@ -1,3 +1,10 @@
 <?php
 
-phpinfo();
+$scriptDir = dirname($_SERVER['SCRIPT_FILENAME']);
+$repoDir = realpath($scriptDir . '/..');
+
+chdir($repoDir);
+$output = [];
+exec("git pull origin master", $output);
+echo implode("\n", $output);
+
